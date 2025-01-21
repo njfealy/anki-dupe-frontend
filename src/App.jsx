f18d17a10a693d51f6ab276fc8534030b779eff6
njfealy <njfealy@gmail.com>
2025-01-21 13:01:17 -0600

    added basic routing and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import HomePage from "./pages/Home";
-import NavBar from "./components/NavBar";
+import AuthPage from "./pages/Auth";
+import StudyPage from "./pages/Study";
 import "./App.css";
+import RootLayout from "./pages/Root";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/auth", element: <AuthPage /> },
+      { path: "/study", element: <StudyPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
-      <NavBar />
       <RouterProvider router={router} />
     </>
   );
