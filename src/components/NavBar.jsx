1b0b1b5acbcd456e68f040061c18171225a6d431
njfealy <njfealy@gmail.com>
2025-01-23 18:36:44 -0600

    Added user dropdown to NavBar, adjusted Auth styling.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useState } from "react";
 
 import user from "../assets/user.png";
+import settings from "../assets/settings.png"
+import logout from "../assets/logout.png";
 import { authActions } from "../store";
 
 const NavBar = () => {
@@ -10,6 +13,8 @@ const NavBar = () => {
   const auth = useSelector((state) => state.auth.isAuth);
 
   const [isOpen, setIsOpen] = useState(false);
+
+  const location = useLocation();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -36,63 +41,51 @@ const NavBar = () => {
           <NavLink
             to="/study"
             // className={({ isActive }) => (isActive ? "text-slate-500" : "")}
-            className="rounded px-5
+            className="rounded px-4 pt-4 font-semibold text-gray-600
             after:block after:border-b-2 after:border-gray-400
-            after:scale-x-0 after:transition after:duration-200 after:ease-out
-            hover:after:scale-x-150 hover:bg-gray-200"
+            after:scale-x-0 transition after:duration-200 after:ease-out
+            hover:after:scale-x-150 hover:bg-gray-200
+            "
           >
             Study
           </NavLink>
         </div>
 
-        <div className="flex gap-3 items-end m-2 mx-3">
-          {!auth ? (
-            <NavLink
-              to="/auth"
-              className="rounded-lg px-4 py-2 mb-1 text-white bg-blue-700 transition hover:bg-violet-700 hover:duration-200"
-            >
-              Log in
-            </NavLink>
-          ) : (
-            <>
-              <img
-                src={user}
-                className="max-h-5 max-w-5 m-1"
-                onClick={toggleDropdown}
-              />
-              {isOpen && (
-                <ul>
-                  <li onClick={logoutHandler} className="relative">
-                    Logout
-                  </li>
-                </ul>
-              )}
-            </>
-          )}
-          {/* {auth ? (
-              <span>
+        {location.pathname != "/auth" ? (
+          <div className="flex gap-3 items-end m-2 mx-3">
+            {!auth ? (
+              <NavLink
+                to="/auth"
+                className="rounded-lg px-4 py-2 mb-1 font-semibold text-white bg-blue-700 transition hover:bg-violet-700 hover:duration-200"
+              >
+                Log in
+              </NavLink>
+            ) : (
+              <div>
                 <img
                   src={user}
-                  className="max-h-5 max-w-5 m-1"
+                  className="max-h-5 max-w-5 m-3 hover:opacity-50 transition duration:200"
                   onClick={toggleDropdown}
                 />
                 {isOpen && (
-                  <ul>
-                    <li onClick={logoutHandler} className="relative">
-                      Logout
+                  <ul className="z-10 absolute right-0 top-16 w-48 bg-white rounded-l-lg py-3">
+                    <li className="font-semibold p-3 flex items-center hover:bg-gray-300 transition duration:200">
+                      <img src={settings} className="size-4 ml-3" />
+                      <button className="px-3">Settings</button>
+                    </li>
+                    <li
+                      onClick={logoutHandler}
+                      className="font-semibold p-3 flex items-center hover:bg-gray-300 transition duration:200"
+                    >
+                      <img src={logout} className="size-3.5 ml-3" />
+                      <button className="px-3">Log out</button>
                     </li>
                   </ul>
                 )}
-              </span>
-            ) : (
-              <NavLink
-                to="/auth"
-                className="p-5 after:block after:border-b-2 after:border-gray-500 after:scale-x-0 after:transition after:duration-200 after:ease-in-out hover:after:scale-50"
-              >
-                Login
-              </NavLink>
-            )} */}
-        </div>
+              </div>
+            )}
+          </div>
+        ) : undefined}
       </nav>
     </>
   );
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -3,6 +3,8 @@ import { authActions } from "../store";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import passwordVisible from "../assets/passwordVisible.png";
+
 const AuthPage = () => {
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth.isAuth);
@@ -10,6 +12,7 @@ const AuthPage = () => {
 
   const [emailInput, setEmailInput] = useState("");
   const [passInput, setPasswordInput] = useState("");
+  const [passVisible, setPassVisible] = useState(false);
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -25,23 +28,70 @@ const AuthPage = () => {
 
   const passInputChangeHandler = (event) => {};
 
+  const passVisToggleHandler = () => {
+    setPassVisible(!passVisible);
+  };
+
   return (
     <>
-      <form onSubmit={submitHandler} className="m-10 flex flex-col">
-        <h1 className="text-2xl">Login</h1>
+      {!auth ? (
+        <div className="flex justify-center">
+          <form
+            onSubmit={submitHandler}
+            className="m-[20vh] flex flex-col gap-5 items-center"
+          >
+            <h1 className="text-3xl text-gray-700 mb-4">Login</h1>
 
-        <label className="text-l inline-block">Email Address</label>
-        <input
-          className=""
-          value={emailInput}
-          onChange={emailInputChangeHandler}
-        />
+            <div className="">
+              <label className="text-lg text-gray-700 font-semibold block">
+                Email Address
+              </label>
+              <input
+                className="w-96 rounded-lg p-2 font-semibold
+          focus:outline-none focus:ring-1 focus:ring-blue-500 
+          transition duration-200 ease-in-out 
+          placeholder-gray-500"
+                placeholder="Enter your email address"
+                value={emailInput}
+                onChange={emailInputChangeHandler}
+              />
+            </div>
 
-        <label className="text-l">Password</label>
-        <input onChange={passInputChangeHandler} />
+            <div className="mb-6">
+              <label className="text-lg text-gray-700 font-semibold block">
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type={!passVisible ? "password" : "text"}
+                  className="w-96 rounded-lg p-2 font-semibold
+          focus:outline-none focus:ring-1 focus:ring-blue-500 
+          transition duration-200 ease-in-out 
+          placeholder-gray-600"
+                  placeholder="Enter your password"
+                  onChange={passInputChangeHandler}
+                />
+                <button
+                  type="button"
+                  onClick={passVisToggleHandler}
+                  className="h-full box-content px-3 absolute inset-y-0 right-0 flex items-center size-5 opacity-60 rounded-r-lg hover:bg-gray-300 transition duration-200"
+                >
+                  <img src={passwordVisible} className="" />
+                </button>
+              </div>
+            </div>
 
-        <button>Login</button>
-      </form>
+            <button
+              type="submit"
+              className="w-96 rounded-lg p-4 mb-1 text-white font-semibold bg-blue-700 transition hover:bg-violet-700 hover:duration-200"
+            >
+              Log in
+            </button>
+          </form>
+        </div>
+      ) : (
+        navigate("/")
+      )}
     </>
   );
 };
