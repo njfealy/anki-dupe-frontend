d727b02cf52304d6e99d5816c50eff8208025d34
njfealy <njfealy@gmail.com>
2025-02-19 23:14:10 -0600

    Added Google OAuth2.0 authentication method.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import GoogleLoginButton from "../components/auth/GoogleLoginButton";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <GoogleLoginButton />
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
